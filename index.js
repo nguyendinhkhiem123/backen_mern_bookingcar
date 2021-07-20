8dfb0d102fe65041fac78c022b3a9954f4a89945
nguyendinhkhiem123 <nguyendinhkhiem12a4@gmail.com>
2021-07-20 15:59:18 +0700

    fix

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const dotenv = require("dotenv")
 const cors = require('cors');
 const Route = require('./Route/index');
 const db = require('./Config/db/index');
-const paypal = require('paypal-rest-sdk');
 
-paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'AR5idgRtmA6JWiQh5tANj_S04daPaSkZDKmc6ZSty3BVsLD2TAVM9A9VYdPpStuVtTkc5cS4npCKY8g1',
-    'client_secret': 'ED4uYT4JOcQ6Y04hyP-NSyPvc67VEU20UcVH-mv4Wcd_AA6gIj8uyvomwNoyDC_IBXCLoRH2PuGU2CNh'
-  });
 dotenv.config()
 
 
